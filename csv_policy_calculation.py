e96e0f0ec8771e87e2f8b3c5c6df859602003a1a
tranalex099 <alextran3115@gmail.com>
2024-04-18 18:11:22 -0400

    progressive tax function

--- csv_policy_calculation.py
+++ csv_policy_calculation.py
@@ -9,34 +9,45 @@ for row in reader:
 def inflation_adjustment(price, original_year, new_year = 2024):
     year = original_year
     new_price = float(price)
-    while int(year) < 2024:
+    while int(year) < new_year:
         new_price = new_price * (1+(float(inflation_dict[year])/100))
         year = str(int(year) + 1)
     return str(int(new_price))
 
-def transaction_tax(price, sale_year, start_year, threshold, percentage):
-    if price > threshold:
-        if sale_year >= start_year:
-            return (price-threshold)*percentage
+property_thresholds = []
+property_percentages = []
+year_property = 2000
+total_property_tax = 0
 
-def propery_tax(price, brackets):
-    tax = 0
-    for bracket in brackets:
+transfer_thresholds = []
+transfer_percentages = []
+year_transfer = 2000
+total_property_tax = 0
+
+def progressive_tax(price, thresholds, percentages):
+    taxes = 0
+    for i in range(len(percentages)):
+        if i == len(thresholds)-1 or price < thresholds[i+1]:
+            taxes += (price-thresholds[i])*[percentages[i]]
+        else:
+            taxes += (thresholds[i+1] - thresholds[i]) * percentages[i]
+    return taxes
         
 
 reader = csv.DictReader(open('boston_residential_sales.csv'))
-new_fields = ['adjusted_price', 'adjusted_mortgage', 'down_payment', 'yearly_payments', 'first_year_payment']
+new_fields = ['transfer_tax', 'property_tax']
 writer = csv.DictWriter(open('policies_boston_residential_sales.csv', 'w'), fieldnames=reader.fieldnames+new_fields)
 writer.writeheader()
 for row in reader:
     sale_year = row['date'][0:4]
-    adjusted_price = inflation_adjustment(row['price'], sale_year)
-    adjusted_mortgage = inflation_adjustment(row['mortgage'], sale_year)
-    row['adjusted_price'] = adjusted_price
-    row['adjusted_mortgage'] = adjusted_mortgage
-    if int(adjusted_mortgage) > int(adjusted_price) or adjusted_mortgage == '0':
-        adjusted_mortgage = str(int(float(adjusted_price) * 80/100))
-    row['down_payment'] = str(int(down_payment(adjusted_price, adjusted_mortgage)))
-    row['yearly_payments'] = str(int(mortgage_payment(adjusted_mortgage)))
-    row['first_year_payment'] = str(int(mortgage_payment(adjusted_mortgage) + down_payment(adjusted_price, adjusted_mortgage)))
+    sale_price = row['price']
+    if sale_year >= year_transfer:
+        transfer_tax = progressive_tax(sale_price, transfer_thresholds, transfer_percentages)
+        adjusted_transfer_tax = inflation_adjustment(transfer_tax, sale_year)
+        total_property_tax += adjusted_transfer_tax
+    else:
+        transfer_tax = 0
+    row['transfer_tax'] = transfer_tax
+    yr = year_property
+    while yr <= curr_year:
     writer.writerow(row)
