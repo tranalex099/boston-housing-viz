a093b7259f0de64017d0a10c8ed4bd730f40bd70
tranalex099 <alextran3115@gmail.com>
2024-04-25 18:05:26 -0400

    negative tax values

--- csv_policy_calculation.py
+++ csv_policy_calculation.py
@@ -14,13 +14,13 @@ def inflation_adjustment(price, original_year, new_year = 2024):
         year = str(int(year) + 1)
     return str(int(new_price))
 
-property_thresholds = [0]
-property_percentages = [0]
+property_thresholds = [500000, 1000000]
+property_percentages = [0.03, 0.06]
 year_property = 2000
 total_property_tax = 0
 
-transfer_thresholds = [0]
-transfer_percentages = [0]
+transfer_thresholds = [1000000]
+transfer_percentages = [0.05]
 year_transfer = 2000
 total_transfer_tax = 0
 
@@ -28,7 +28,7 @@ def progressive_tax(price, thresholds, percentages):
     taxes = 0
     for i in range(len(percentages)):
         if i == len(thresholds)-1 or price < thresholds[i+1]:
-            taxes += (price-thresholds[i])*[percentages[i]]
+            taxes += (price-thresholds[i])*(percentages[i])
         else:
             taxes += (thresholds[i+1] - thresholds[i]) * percentages[i]
     return taxes
@@ -41,24 +41,27 @@ writer.writeheader()
 for row in reader:
     sale_year = row['date'][0:4]
     sale_price = row['price']
-    if sale_year >= year_transfer:
-        transfer_tax = progressive_tax(sale_price, transfer_thresholds, transfer_percentages)
+    if int(sale_year) >= year_transfer:
+        transfer_tax = progressive_tax(int(sale_price), transfer_thresholds, transfer_percentages)
         adjusted_transfer_tax = inflation_adjustment(transfer_tax, sale_year)
-        total_transfer_tax += adjusted_transfer_tax
+        total_transfer_tax += int(adjusted_transfer_tax)
     else:
         transfer_tax = 0
-    row['transfer_tax'] = transfer_tax
-    year_built = row['yearbuilt']
-    if year_built < year_property:
-        yr = year_property
+    row['transfer_tax'] = adjusted_transfer_tax
+    if row['yearbuilt'] == '':
+        year_built = 0
     else:
-        yr = year_built
+        year_built = int(row['yearbuilt'])
+    if year_built < year_property:
+        yr = str(year_property)
+    else:
+        yr = str(year_built)
     prop_tax = 0
-    while yr <= 2024:
-        yr_val = inflation_adjustment(sale_price, sale_year, yr)
-        yr_prop_tax = progressive_tax(yr_val, property_thresholds, property_percentages)
-        prop_tax += inflation_adjustment(yr_prop_tax, yr, 2024)
-        yr += 1
+    while int(yr) <= 2024:
+        yr_val = inflation_adjustment(sale_price, sale_year, int(yr))
+        yr_prop_tax = progressive_tax(int(yr_val), property_thresholds, property_percentages)
+        prop_tax += int(inflation_adjustment(yr_prop_tax, yr, 2024))
+        yr += str(int(yr)+1)
     total_property_tax += prop_tax
     row['property_tax'] = prop_tax
     writer.writerow(row)
